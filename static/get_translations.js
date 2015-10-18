b4d727c0cd08ab3bfb0b766f4dd6476376fa481d
Allan Simon <allan.simon@supinfo.com>
2015-10-19 04:35:10 +0800

    close #10 now we can get a sentence and its translation

---
+++ static/get_translations.js
@@ -0,0 +1,46 @@
+var GetTranslations = (function () {
+    'use strict';
+
+
+    /**
+     * @param {string} id
+     * @param {Element} translationsUL
+     */
+    var load = function (id, translationsUL) {
+        Async.get('/sentences/' + id + '/translations/sentences')
+            .then(
+                /**
+                 * @param {Array.<Object>} translations
+                 */
+                function(translations) {
+                    translations.forEach(
+                        /**
+                         * @param {Object}
+                         */
+                        function(oneTranslation) {
+                            var li = document.createElement("li");
+
+                            var sentenceSpan = document.createElement("span");
+                            var text = document.createTextNode(oneTranslation.content);
+                            sentenceSpan.appendChild(text);
+                            li.appendChild(sentenceSpan);
+
+                            var show = document.createElement("a");
+                            show.href = "show_sentence.html?id=" + oneTranslation.id;
+                            show.appendChild(document.createTextNode("show"));
+                            li.appendChild(show);
+
+                            translationsUL.appendChild(li);
+                        }
+                    );
+                    console.log(translations);
+                }
+            )
+            .catch(function() { translationsUL.innerHTML = 'Error';})
+        ;
+    };
+
+    return {
+        load: load
+    };
+} ());
